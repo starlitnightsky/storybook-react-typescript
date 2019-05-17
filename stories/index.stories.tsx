e6a2d11e52a4d6594b2d6dee1ef63b1ae9dc6c3d
starlitnightsky <miraclehourglass@gmail.com>
2019-05-17 16:34:08 +0100

    Reformat code

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,12 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 
-storiesOf('Stuff', module).add(
-  'Hello',
-  () => {
-    return (
-            <h1>Hello</h1>
-    )
-  }
-)
-
+storiesOf('Stuff', module).add('Hello', () => {
+  return <h1>Hello</h1>
+})
